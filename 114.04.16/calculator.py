5e92374e63a301c8e1ed4d58cc92e4e40bb84943
Chago <black789563@gmail.com>
2025-04-16 11:54:54 +0800

    基本計算 print 功能 (字串 + 數字)

---
+++ 114.04.16/calculator.py
@@ -0,0 +1,13 @@
+# 建立計算機
+# 測試
+# name = input("請輸入姓名: ")
+# age = input("請輸入年紀: ")
+# print("哈囉" + name + "你今年" + age + "歲")
+
+# 計算機
+number1 = input("請輸入第一個數字: ")
+number2 = input("請輸入第二個數字: ")
+# 方法 1 (用於整數計算)
+# print(int(number1) + int(number2))
+# 方法 2 (可用小數)
+print(float(number1) + float(number2))
